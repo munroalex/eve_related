baa5577444e39dc2de7972e65d8430fd2da6fac3
Alex <munroa.alex@gmail.com>
2020-11-07 17:04:12 +0800

    Added more request templates

--- scripts/sso-login.py
+++ scripts/sso-login.py
@@ -29,13 +29,29 @@ security.verify()
 ###---Wallet---###
 ########################
 
-op = app.op['get_characters_character_id_wallet'](
+wallet_balance = app.op['get_characters_character_id_wallet'](
     character_id=api_info['sub'].split(':')[-1]
 )
-wallet = client.request(op)
+wallet = client.request(wallet_balance)
 
 # and to see the data behind, let's print it
 print(wallet.data)
+
+
+get_transactions = app.op['get_characters_character_id_wallet_transactions'](
+    character_id=api_info['sub'].split(':')[-1]
+)
+transactions = client.request(get_transactions)
+# this should give a set of dictionaries of individual transactions
+print(transactions.data) 
+
+
+get_journal = app.op['get_characters_character_id_wallet_journal'](
+    character_id=api_info['sub'].split(':')[-1]
+)
+journal = client.request(get_journal)
+# this should give a set of dictionaries of individual jounral entries
+print(journal.data) 
 
 ########################
 ###---Portrait---###
@@ -62,7 +78,7 @@ corp_history = client.request(get_corp_id)
 print(corp_history.data)
 
 
-get_corp_name = app.op[''get_corporations_corporation_id'](
+get_corp_name = app.op['get_corporations_corporation_id'](
     corporation_id='The Corp Id from the previous request'
 )
 corp_deets = client.request(get_corp_name)
@@ -80,28 +96,112 @@ print(logo.data)
 ########################
 ###---Alliance---###
 ########################
+#Get alliance ID
 get_alliance_id = app.op['get_corporations_corporation_id_alliancehistory'](
     corporation_id='The Corp Id from the previous request'
 )
 alliance_history = client.request(get_alliance_id)
-
 # this should give a list of dictionaries including corp IDs
 print(alliance_history.data)
 
-
+                       
+#get Alliance Name
 get_alliance_name = app.op[''get_alliances_alliance_id'](
     alliance_id='The alliance Id from the previous request'
 )
 alliance_deets = client.request(get_alliance_name)
-
 # this should give a dictionary with all details of the corporation
 print(alliance_deets.data)
-                       
+                           
+                           
+#Get alliance Logo                       
 alliance_icon = app.op['get_alliances_alliance_id_icons'](
     corporation_id = 'The alliance Id from the previous request'
 )
 alliance_logo = client.request(alliance_icon)
 # this will return a dictionary of links to different sized corp logos
-print(alliance_logo.data)
-                       
-                       
+print(alliance_logo.data)              
+########################
+###---Skills---###
+########################
+get_skill_points = app.op['get_characters_character_id_skills'](
+    character_id=api_info['sub'].split(':')[-1]
+)
+skill_list = client.request(get_skill_points)
+# this should give a dictionary of a list of dictionaries with individual skills and total sp including unallocated
+print(skill_list.data)  
+                           
+get_skill_queue = app.op['get_characters_character_id_skillqueue'](
+    character_id=api_info['sub'].split(':')[-1]
+)
+skill_queue = client.request(get_skill_queue)
+# this should give a bunch of dictionaries including start and finish times for skill training
+print(skill_queue.data)
+                           
+########################
+###---Status---###
+########################
+op = app.op['status']()
+status = client.request(op)
+#returns a dictionary including player count 
+print(status.data)
+
+                           
+                           
+########################
+###---Notifications---###
+########################                           
+get_notifications = app.op['get_characters_character_id_notifications'](
+    character_id=api_info['sub'].split(':')[-1]
+)
+notifications = client.request(get_notifications)
+# this should give a bunch of dictionaries of notifications
+print(notifications.data)                           
+                           
+                           
+########################
+###---Location---###
+########################                           
+#get player location
+get_player_location = app.op['get_characters_character_id_location'](
+    character_id=api_info['sub'].split(':')[-1]
+)
+location = client.request(get_player_location)
+# this should give a dictionary with solar system and station
+print(location.data)
+                           
+#get ship type                           
+get_ship = app.op['get_characters_character_id_ship'](
+    character_id=api_info['sub'].split(':')[-1]
+)
+ship_type = client.request(get_ship)
+# this should give a dictionary with ship item id, name and type id
+print(ship_type.data)                           
+                           
+########################
+###---Mail---###
+########################
+#get player mail ids
+get_mail_ids = app.op['get_characters_character_id_mail'](
+    character_id=api_info['sub'].split(':')[-1]
+)
+mail_ids = client.request(get_mail_ids)
+# this should give a dictionary with solar system and station
+print(mail_ids.data)                           
+                           
+#get return a single mail
+get_mails = app.op['get_characters_character_id_mail_mail_id'](
+    character_id=api_info['sub'].split(':')[-1],
+    mail_id='get mail id from previous request'
+)
+mail_body = client.request(get_mails)
+# this should give a dictionary with solar system and station
+print(mail_body.data)                           
+                           
+                           
+               
+                           
+                           
+                           
+                           
+                           
