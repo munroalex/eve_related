edcf3520773dd0b59dc669d38dff96df05c654da
Alex <munroa.alex@gmail.com>
2020-11-08 10:57:33 +0800

    Update updated_reprocessing.py

--- scripts/updated_reprocessing.py
+++ scripts/updated_reprocessing.py
@@ -12,6 +12,8 @@ client = EsiClient(
     headers={'User-Agent': 'Something CCP can use to contact you and that define your app'},
     raw_body_only=False,  # default False, set to True to never parse response and only return raw JSON string content.
 )
+#create empty dictionary for all prices and volumes
+price_list = {}
 
 #function for getting the minimum price of items in Jita
 def get_min_jita_price(item):
@@ -21,7 +23,7 @@ def get_min_jita_price(item):
 
     #create request to swagger interface
 
-###Forge Region Request###
+    ###Forge Region Request###
     market_order_operation = app.op['get_markets_region_id_orders'](
         region_id=10000002,
         type_id=type_id,
@@ -55,37 +57,37 @@ def get_min_jita_price(item):
     else:
         print('Request error: ' + res.status)
     
-#create empty dictionary for all prices and volumes
-price_list = {}
-#loop through items and grab prices and volumes
-for key in type_ids:
-    get_min_jita_price(key)
 
-for ore in ore_list:
-    try:
-        total_mineral_value = []
-        ore_value = price_list[ore]["Price"] * 100
-        total_mineral_mass = []
-        for key in reprocessed_ores[ore]:
-            mineral_price = price_list[key]["Price"]
-            mineral_value = mineral_price * reprocessed_ores[ore][key] * reprocessing_yield
-            mineral_mass = reprocessed_ores[ore][key] * reprocessing_yield * volumes[key] * price_list[ore]["Volume"]
-        
-            total_mineral_mass.append(mineral_mass)
-            total_mineral_value.append(mineral_value)
-        profit = (price_list[ore]["Volume"] / 100) * ((sum(total_mineral_value) - (sum(total_mineral_value) * broker_fee) - (sum(total_mineral_value) * sales_tax)) - ore_value)
-        if profit > 25000000:
-            print('Ore to purchase: ' + ore)
-            print('Volume to purchase: ' + '{:,.0f}'.format(price_list[ore]["Volume"]))
-            print('Max purchase price: $' + '{:,.2f}'.format(price_list[ore]["Price"]))
-            print('Total cost: $' + '{:,.2f}'.format(price_list[ore]["Volume"] * price_list[ore]["Price"]))
-            print("Profit: $" + '{:,.2f}'.format(profit))
+def find_profit():
+    for ore in ore_list:
+        try:
+            total_mineral_value = []
+            ore_value = price_list[ore]["Price"] * 100
+            total_mineral_mass = []
             for key in reprocessed_ores[ore]:
-                print('Min sell price: ' + key + ': $' + '{:,.2f}'.format(price_list[key]["Price"]))
-            print('------------------------------')
+                mineral_price = price_list[key]["Price"]
+                mineral_value = mineral_price * reprocessed_ores[ore][key] * reprocessing_yield
+                mineral_mass = reprocessed_ores[ore][key] * reprocessing_yield * volumes[key] * price_list[ore]["Volume"]
+        
+                total_mineral_mass.append(mineral_mass)
+                total_mineral_value.append(mineral_value)
+            profit = (price_list[ore]["Volume"] / 100) * ((sum(total_mineral_value) - (sum(total_mineral_value) * broker_fee) - (sum(total_mineral_value) * sales_tax)) - ore_value)
+            if profit > 25000000:
+                print('Ore to purchase: ' + ore)
+                print('Volume to purchase: ' + '{:,.0f}'.format(price_list[ore]["Volume"]))
+                print('Max purchase price: $' + '{:,.2f}'.format(price_list[ore]["Price"]))
+                print('Total cost: $' + '{:,.2f}'.format(price_list[ore]["Volume"] * price_list[ore]["Price"]))
+                print("Profit: $" + '{:,.2f}'.format(profit))
+                for key in reprocessed_ores[ore]:
+                    print('Min sell price: ' + key + ': $' + '{:,.2f}'.format(price_list[key]["Price"]))
+                print('------------------------------')
             
-    except:
-        print('------------------------------')
-        print('No ' + ore + ' in Jita.')
-        print('------------------------------')
+        except:
+            print('------------------------------')
+            print('No ' + ore + ' in Jita.')
+            print('------------------------------')
 
+if __name__ == "__main__": 
+    for key in type_ids:
+       get_min_jita_price(key)
+    find_profit()
