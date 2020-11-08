df08dcd7787937be035a2c84b732eb1727c9de63
Alex <munroa.alex@gmail.com>
2020-11-08 10:45:52 +0800

    Updated error handling

--- scripts/updated_reprocessing.py
+++ scripts/updated_reprocessing.py
@@ -17,35 +17,43 @@ client = EsiClient(
 def get_min_jita_price(item):
     #get type_id of mineral input
     type_id = type_ids.get(item)
+
+
     #create request to swagger interface
+
+###Forge Region Request###
     market_order_operation = app.op['get_markets_region_id_orders'](
         region_id=10000002,
         type_id=type_id,
         order_type='sell',
     )
-    #make request
-    response = client.request(market_order_operation, raw_body_only=True)
-    #limit orders to Jita station
-    station = []
-    orders = json.loads(response.raw)
-    for order in orders:
-        if order["location_id"] == 60003760:
-            station.append(order)
-    # Find minimum sell price
-    try:
-        price = [x['price'] for x in station]
-        minimum_price = min(price)
-        #find total volume within 5% of minimum price
-        volume = []
-        for order in station:
-            if order['price'] <= minimum_price*1.05:
-                volume.append(order['volume_remain'])
-        sum_volume = sum(volume)
-    
-        #add item price and volume to dictionary
-        price_list[item] = {"Price":minimum_price,"Volume":sum_volume}
-    except:
-        pass
+    res = client.head(market_order_operation)
+    #if status 200 all good to go
+    if res.status == 200:
+        #make request
+        response = client.request(market_order_operation, raw_body_only=True)
+        #limit orders to Jita station
+        station = []
+        orders = json.loads(response.raw)
+        try:
+            for order in orders:
+                if order["location_id"] == 60003760:
+                    station.append(order)
+            # Find minimum sell price
+            price = [x['price'] for x in station]
+            minimum_price = min(price)
+            #find total volume within 5% of minimum price
+            volume = []
+            for order in station:
+                if order['price'] <= minimum_price*1.05:
+                    volume.append(order['volume_remain'])
+            sum_volume = sum(volume)
+            #add item price and volume to dictionary
+            price_list[item] = {"Price":minimum_price,"Volume":sum_volume}
+        except:
+            print('No ' + item + ' on market.')    
+    else:
+        print('Request error: ' + res.status)
     
 #create empty dictionary for all prices and volumes
 price_list = {}
@@ -77,5 +85,7 @@ for ore in ore_list:
             print('------------------------------')
             
     except:
-        pass
+        print('------------------------------')
+        print('No ' + ore + ' in Jita.')
+        print('------------------------------')
 
