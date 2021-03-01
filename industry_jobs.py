21420bd6792054d3a7e68880abfaa183dd19b635
Alex <munroa.alex@gmail.com>
2021-03-01 08:55:55 +0800

    tidy up script
    Still need to add in reactions jobs

--- industry_jobs.py
+++ industry_jobs.py
@@ -1,67 +1,65 @@
+#iterate through a dataframe of character tokens, only for use in testing. Use database in production.
 for index, row in df.iterrows():
     security.update_token({
     'access_token': '',  # leave this empty
     'expires_in': -1,  # seconds until expiry, so we force refresh anyway
     'refresh_token': df['refresh_token'][index]
 })
+    #Get skill points will return what skills a character has trained to what level.
     get_skill_points = app.op['get_characters_character_id_skills'](
         character_id=df['character_id'][index]
     )
     skill_list = client.request(get_skill_points)
-    #print(skill_list.data)
     # this should give a dictionary of a list of dictionaries with individual skills and total sp including unallocated
     total_sp = skill_list.data['total_sp']
-    ##Manufacturing
-    job_slots = 1
+    ##Calculate available manufacturing slots
+    manufacturing_job_slots = 1
     manufacturing_jobs = 0
     for skill in skill_list.data['skills']:
         if skill['skill_id'] == 3387:
             if skill['active_skill_level'] != 0:
-                job_slots = job_slots + skill['trained_skill_level']
+                manufacturing_job_slots = manufacturing_job_slots + skill['trained_skill_level']
         if skill['skill_id'] == 24625:
             if skill['active_skill_level'] != 0:
-                job_slots = job_slots + skill['trained_skill_level']
-    print(df['Name'][index])
-    
-
-    #/characters/{character_id}/industry/jobs/
-    get_industry_jobs = app.op['get_characters_character_id_industry_jobs'](
-        character_id=df['character_id'][index]
-    )
-    jobs_list = client.request(get_industry_jobs)
-    job_list_json = json.loads(jobs_list.raw)
-    print("Manufacturing slots:")
-    if job_list_json:
-        for job in job_list_json:
-            if job['activity_id'] == 1:
-                manufacturing_jobs += 1
-        print(job_slots - manufacturing_jobs)
-    else:
-        print(job_slots)
-    ##Science
-    job_slots = 1
+                manufacturing_job_slots = manufacturing_job_slots + skill['trained_skill_level']
+    ##Calculate available science slots
+    science_job_slots = 1
     science_jobs = 0
     for skill in skill_list.data['skills']:
         if skill['skill_id'] == 3406:
             if skill['active_skill_level'] != 0:
-                job_slots = job_slots + skill['trained_skill_level']
+                science_job_slots = science_job_slots + skill['trained_skill_level']
         if skill['skill_id'] == 24624:
             if skill['active_skill_level'] != 0:
-                job_slots = job_slots + skill['trained_skill_level']
-    
+                science_job_slots = science_job_slots + skill['trained_skill_level']    
     
 
-    #/characters/{character_id}/industry/jobs/
+    #Now we grab a list of active jobs that character has
     get_industry_jobs = app.op['get_characters_character_id_industry_jobs'](
         character_id=df['character_id'][index]
     )
     jobs_list = client.request(get_industry_jobs)
     job_list_json = json.loads(jobs_list.raw)
-    print("Science slots:")
+    #if there are jobs in the list we will print out how many slots are available, else we will print out the total slots available
     if job_list_json:
         for job in job_list_json:
-            if job['activity_id'] != 1:
-                science_jobs +=1
-        print(job_slots - science_jobs)
+            if job['activity_id'] == 1:
+                manufacturing_jobs += 1
+            else:
+                science_jobs += 1
+                
+        print(df['Name'][index])
+        print("Manufacturing slots:")
+        print(manufacturing_job_slots - manufacturing_jobs)
+        print("Science slots:")
+        print(science_job_slots - science_jobs)
     else:
-        print(job_slots) 
+        print(df['Name'][index])
+        print("Manufacturing slots:")
+        print(manufacturing_job_slots)
+        print("Science slots:")
+        print(science_job_slots)
+
+    
+    
+
